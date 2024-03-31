aa081cc9ecc66446b3f775d992f2d1bedf8b7d4d
pstpn <stepaha78@gmail.com>
2024-04-01 00:01:12 +0300

    [*]: finish company storage impl

--- src/internal/storage/postgres/company.go
+++ src/internal/storage/postgres/company.go
@@ -19,6 +19,30 @@ func NewCompanyStorage(db *postgres.Postgres) storage.CompanyStorage {
 	return &companyStorageImpl{db}
 }
 
+func (c *companyStorageImpl) Create(ctx context.Context, request *dto.CreateCompanyRequest) error {
+	query := c.Builder.
+		Insert(companyTable).
+		Columns(
+			nameField,
+			cityField,
+		).
+		Values(
+			request.Name,
+			request.City,
+		)
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return err
+	}
+	_, err = c.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (c *companyStorageImpl) GetByID(ctx context.Context, request *dto.GetCompanyRequest) (*model.Company, error) {
 	query := c.Builder.
 		Select(
@@ -42,4 +66,21 @@ func (c *companyStorageImpl) GetByID(ctx context.Context, request *dto.GetCompan
 	}
 
 	return &company, nil
+}
+
+func (c *checkpointStorageImpl) Delete(ctx context.Context, request *dto.DeleteCompanyRequest) error {
+	query := c.Builder.
+		Delete(companyTable).
+		Where(squirrel.Eq{idField: request.CompanyID})
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return err
+	}
+	_, err = c.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
